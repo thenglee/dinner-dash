dfc3b77b3f1e25ace7f21a20665b1117f2852641
Tan Theng Lee <thengleetan@gmail.com>
2015-11-23 18:46:20 +0800

    Add specs for item's presence validation

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,6 +7,27 @@ RSpec.describe Item, type: :model do
     expect(item.valid?).to eq true
   end
 
-  describe "validations"
+  describe "validations" do
+    it "is invalid without a name" do
+      item = build(:item, name: nil)
+      expect(item.valid?).to eq false
+    end
+
+    it "is invalid without a description" do
+      item = build(:item, description: nil)
+      expect(item.valid?).to eq false
+    end
+
+    it "is invalid without a price" do
+      item = build(:item, price: nil)
+      expect(item.valid?).to eq false
+    end
+
+    it "is invalid without available" do
+      item = build(:item, available: nil)
+      expect(item.valid?).to eq false
+    end
+  end
+
   describe "associations"
 end
