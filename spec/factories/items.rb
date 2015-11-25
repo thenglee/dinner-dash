3b73aeca0c0d6c8b092e8efd697c20277f50661c
Mingding <hanmd82@gmail.com>
2015-11-26 00:49:06 +0800

    Fix unit tests for Item model and remove excess factory methods

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,15 +4,5 @@ FactoryGirl.define do
     description 'Delicious chicken pad thai with peanuts and chilli flakes by the side.'
     price 12.5
     available true
-
-    factory :item_with_categories do
-      transient do
-        categories_count 1
-      end
-
-      after(:create) do |item, evaluator|
-        create_list(:categories_items, evaluator.categories_count, item: item)
-      end
-    end
   end
 end
